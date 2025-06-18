c52e69d62ce74683fff625151a327aab30cbad7b
2IMT <daniil_t04@mail.ru>
2025-06-18 15:15:16 +0300

    refactor: update jwt functions to accept user_id

--- backend/app/services/jwt.py
+++ backend/app/services/jwt.py
@@ -9,34 +9,37 @@ from typing import Dict, Any
 ALGORITHM = "HS256"
 
 
-def create_access_token(data: dict) -> str:
+def create_access_token(user_id: int) -> str:
     """Create new access token."""
     settings = get_settings()
 
-    to_encode = data.copy()
+    to_encode: dict = { "sub": str(user_id) }
     expire = datetime.utcnow() \
         + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_KEY, algorithm=ALGORITHM)
 
 
-def create_refresh_token(data: dict) -> str:
+def create_refresh_token(user_id: int) -> str:
     """Create new refresh token."""
     settings = get_settings()
 
     expire = datetime.utcnow() \
         + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
-    to_encode = data.copy()
+    to_encode: dict = { "sub": str(user_id) }
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_KEY, algorithm=ALGORITHM)
 
 
-def verify_token(token: str) -> None | Dict[str, Any]:
-    """Verify access token."""
+def verify_token(token: str) -> None | int:
+    """Verify access token and get user ID."""
     settings = get_settings()
 
     try:
         payload = jwt.decode(token, settings.JWT_KEY, algorithms=[ALGORITHM])
-        return payload
+        user_id = payload.get("sub")
+        if not user_id:
+            return None
+        return int(user_id)
     except JWTError:
         return None
