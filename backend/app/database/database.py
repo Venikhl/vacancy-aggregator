16adc806923b1fa2aac009f85498d55e11f48e0a
2IMT <daniil_t04@mail.ru>
2025-06-18 14:01:54 +0300

    refactor: use database URL from project config

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,15 +3,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import AsyncGenerator
-import os
-from dotenv import load_dotenv
+from app.core.config import get_settings
 
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(get_settings().DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession,
                                    expire_on_commit=False)
 
