2bb4833adb816427f808e7c49d3157c8f7b994a6
2IMT <daniil_t04@mail.ru>
2025-07-09 10:02:36 +0300

    feat: add HH_CLIENT_ID and HH_CLIENT_SECRET to project config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -19,6 +19,8 @@ class Settings(BaseSettings):
     PROFILE_PICTURE_DIRECTORY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
+    HH_CLIENT_ID: str
+    HH_CLIENT_SECRET: str
 
 
 @lru_cache()
