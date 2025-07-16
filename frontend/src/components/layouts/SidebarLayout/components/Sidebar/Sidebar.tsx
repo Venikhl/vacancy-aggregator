10fdde05771a3d11fa9a023b8de671c082c9c96d
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-07-17 00:24:16 +0300

    feat: add updating photos from profile

--- frontend/src/components/layouts/SidebarLayout/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/layouts/SidebarLayout/components/Sidebar/Sidebar.tsx
@@ -29,7 +29,14 @@ const Sidebar: React.FC = () => {
         >
             <div className="flex flex-col items-center mb-8 px-4">
                 <img
-                    src={value?.profile_pic_url || '/planet.png'}
+                    src={
+                        value
+                            ? value.profile_pic_url.replace(
+                                  '127.0.0.1',
+                                  'http://localhost:5173',
+                              )
+                            : '/planet.png'
+                    }
                     alt="avatar"
                     className="w-20 h-20 rounded-full object-cover mb-4"
                 />
